0661479d27673f4400b07ef3bc8b930368c210c6
MatthieuRbc <104311607+MatthieuRbc@users.noreply.github.com>
2022-05-05 19:31:54 +0200

    Update Accueil_admin.php

--- projet/Accueil_admin.php
+++ projet/Accueil_admin.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['utilisateur'])){
+if(!isset($_SESSION['utilisateur']) || $_SESSION['utilisateur'] != "admin"){
   header('Location: Page_Connexion.php');
 }
 else{?>
@@ -8,11 +8,12 @@ else{?>
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Espace Personnel Admin</title>
   </head>
   <body>
      <p><a href="Membres.php">Afficher tous les membres</a></p>
      <p><a href="Avis.php">Afficher tous les avis</a></p>
+     <p><a href="Afficher_Sites.php">Aller à l'accueil</a></p>
      <p><a href="Deconnexion.php"><button>Déconnexion</button></a></p>
   </body>
 </html>
